c28dc92ceb777cba6e1dfd29d1f38187f6cbdab6
Unknown <chris.kinsella@helkinsoft.com>
2020-01-10 14:47:31 +0000

    updated region settings config script

--- scripts/windows/iaps/SetRegionalConfig.ps1
+++ scripts/windows/iaps/SetRegionalConfig.ps1
@@ -1,3 +1,57 @@
-Set-WinSystemLocale -SystemLocale en-GB
-Set-WinHomeLocation -GeoId 242
-Set-WinUserLanguageList -LanguageList (New-WinUserLanguageList -Language en-GB) -Force 
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "Continue"
+
+Write-Host ('==================================================')
+Write-Host ('Updating Region Config')
+Write-Host ('==================================================')
+
+Write-Host ('---------region config before update-------------')
+try {
+    Get-WinSystemLocale
+    Get-WinHomeLocation
+    Get-WinUserLanguageList
+}
+catch [Exception] {
+    Write-Host ('Exception getting region config')
+    echo $_.Exception|format-list -force
+    exit 1
+}
+
+try {
+    Set-WinSystemLocale -SystemLocale en-GB
+}
+catch [Exception] {
+    Write-Host ('Set-WinSystemLocale: Exception')
+    echo $_.Exception|format-list -force
+    exit 1
+}
+
+try {
+    Set-WinHomeLocation -GeoId 242
+}
+catch [Exception] {
+    Write-Host ('Set-WinHomeLocation:Exception')
+    echo $_.Exception|format-list -force
+    exit 1
+}
+
+try {
+    Set-WinUserLanguageList -LanguageList (New-WinUserLanguageList -Language en-GB) -Force 
+}
+catch [Exception] {
+    Write-Host ('Set-WinUserLanguageList:Exception')
+    echo $_.Exception|format-list -force
+    exit 1
+}
+
+Write-Host ('---------region config post update-------------')
+try {
+    Get-WinSystemLocale
+    Get-WinHomeLocation
+    Get-WinUserLanguageList
+}
+catch [Exception] {
+    Write-Host ('Exception getting region config')
+    echo $_.Exception|format-list -force
+    exit 1
+} 
