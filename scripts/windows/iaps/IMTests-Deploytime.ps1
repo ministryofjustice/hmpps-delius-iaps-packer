340c9e01f15f08418f6e9b4a669dd4598a47953e
Unknown <chris.kinsella@helkinsoft.com>
2020-02-12 17:28:20 +0000

    Added tests to confirm OpenSSH Server Service is Disabled and Stopped

--- scripts/windows/iaps/IMTests-Deploytime.ps1
+++ scripts/windows/iaps/IMTests-Deploytime.ps1
@@ -470,5 +470,17 @@ Describe 'DNS Search Suffix Configuration' {
     } 
 
 }
- 
+
+Describe 'Other Windows Services' {
+    Describe 'sshd (OpenSSH Server) is Stopped' {
+        Service sshd Status { Should Be Stopped }
+    }
+    Describe 'sshd (OpenSSH Server) is Stopped' {
+        Service sshd Status { Should Be Stopped }
+    } 
+    It 'sshd (OpenSSH Server) Service is Disabled' {
+        $service = Get-Service | Where { $_.Name -eq 'sshd' }
+        $service.StartType | Should Be 'Disabled'
+    }
+}
 
