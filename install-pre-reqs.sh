ac2c242a52e5c60aa67bb7d9f0af6eefb68f46c1
Unknown <chris.kinsella@helkinsoft.com>
2020-02-14 17:01:33 +0000

    Removed sudo from pre-reqs command

--- install-pre-reqs.sh
+++ install-pre-reqs.sh
@@ -3,34 +3,34 @@
 # Docker for Ubuntu
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
 
-echo 'apt-get update'
-sudo apt-get update -y
+echo 'Running: apt-get update'
+apt-get update -y
 
-echo 'apt-get install'
-sudo apt-get install -y \
+echo 'Running: apt-get install'
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 
-echo 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+echo 'Running: curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo 'sudo apt-key fingerprint 0EBFCD88'
-sudo apt-key fingerprint 0EBFCD88
+echo 'Running: apt-key fingerprint 0EBFCD88'
+apt-key fingerprint 0EBFCD88
 
-echo 'apt-get update'
-sudo add-apt-repository \
+echo 'Running: apt-get update'
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-echo 'apt-get update'
-sudo apt-get update -y
+echo 'Running: apt-get update'
+apt-get update -y
 
-echo 'sudo apt-get install -y docker-ce docker-ce-cli containerd.io'
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+echo 'Running: apt-get install -y docker-ce docker-ce-cli containerd.io'
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
-echo 'sudo docker run hello-world'
-sudo docker run hello-world
+echo 'Running: docker run hello-world'
+docker run hello-world
