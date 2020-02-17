f06cf2bd100a66b6a146f3a6b8a87853738458bd
Unknown <chris.kinsella@helkinsoft.com>
2020-02-17 13:35:47 +0000

    Removed sudo command

--- install-pre-reqs.sh
+++ install-pre-reqs.sh
@@ -50,5 +50,5 @@ docker-compose --version
 export VER="1.5.1"
 wget https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
 unzip packer_${VER}_linux_amd64.zip
-sudo mv packer /usr/local/bin
+mv packer /usr/local/bin
 packer --version
