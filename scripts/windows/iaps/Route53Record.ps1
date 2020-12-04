b5410c03b7ef1d33964c86e1e4bc523475f9a080
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-12-04 11:54:01 +0000

    Updated to set R53 records for Host
    ie.
    iaps-admin.delius-stage.internal
    iaps-admin.stage.delius.probation.hmpps.dsd.io
    iaps-admin.stage.probation.service.justice.gov.uk

--- scripts/windows/iaps/Route53Record.ps1
+++ scripts/windows/iaps/Route53Record.ps1
@@ -3,11 +3,26 @@ $VerbosePreference = "Continue"
 
 try {
     $instanceid = Invoke-RestMethod "http://169.254.169.254/latest/meta-data/instance-id"
+    $environment = Get-EC2Tag -Filter @(
+        @{
+            name="resource-id"
+            values="$instanceid"
+        }
+        @{
+            name="key"
+            values="environment"
+        }
+    )
+
     $LANIP = (Get-EC2Instance -Region $region).Instances | ?{$_.InstanceId -eq $instanceid} | select -ExpandProperty PrivateIpAddress
     if (!$LANIP)  {
         Write-Host('Error - R53 Setup failed to retrieve LAN IP')
         Exit 1
     }
+    
+    #=============================================================================
+    # set DNS entry for ecternaldomain - ie. stage.delius.probation.hmpps.dsd.io
+    #=============================================================================
     Set-Location ENV:
     $zoneName =  $env:ExternalDomain + "."
     if (!$zoneName)  {
@@ -20,7 +35,7 @@ try {
         Exit 1
     }
 
-    # Set ResourceRecordSet for v1 of IAPS
+    # Set ResourceRecordSet for v1 of IAPS in 
     if($env:computername -eq 'sim-win-001') {
         $resourceName = "iaps-admin." + $zoneName
     }
@@ -30,7 +45,6 @@ try {
         $resourceName = "iaps-admin-v2." + $zoneName
     }
     
-
     $resourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
     $resourceRecordSet.Name = $resourceName
     $resourceRecordSet.Type = "A"
@@ -53,6 +67,102 @@ try {
 
     # Execute
     Edit-R53ResourceRecordSet -HostedZoneId $hostedZone.id -ChangeBatch_Change $change 
+
+    #=============================================================================
+    # set DNS entry for internaldomain - ie. delius-stage.internal
+    #=============================================================================
+    Set-Location ENV:
+    $internalZoneName =  $env:InternalDomain + "."
+    if (!$internalZoneName)  {
+        Write-Host('Error - R53 Setup failed to zone name from Environment Variable: InternalDomain')
+        Exit 1
+    }
+    $internalHostedZone = Get-R53HostedZones | where Name -eq $internalZoneName
+    if (!$internalHostedZone)  {
+        Write-Host('Error - R53 Setup failed to retrieve internalHostedZone hosted zone data')
+        Exit 1
+    }
+
+    # Set ResourceRecordSet for v1 of IAPS in 
+    if($env:computername -eq 'sim-win-001') {
+        $resourceName = "iaps-admin." + $internalZoneName
+    }
+   
+    # Set ResourceRecordSet for v2 of IAPS
+    if($env:computername -eq 'sim-win-002') {
+        $resourceName = "iaps-admin-v2." + $internalZoneName
+    }
+    
+    $internalResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+    $internalResourceRecordSet.Name = $resourceName
+    $internalResourceRecordSet.Type = "A"
+    $internalResourceRecordSet.ResourceRecords = New-Object Amazon.Route53.Model.ResourceRecord ($LANIP)
+    $internalResourceRecordSet.TTL = 300
+
+    # Set Action
+    if (((Get-R53ResourceRecordSet -HostedZoneId $internalHostedZone.id).ResourceRecordSets | where Name -eq $internalResourceRecordSet | measure).Count -eq 0)
+    {
+        $action = [Amazon.Route53.ChangeAction]::CREATE
+    }
+    else
+    {
+        $action = [Amazon.Route53.ChangeAction]::UPSERT
+    }
+
+    # Set Change 
+    $change = New-Object Amazon.Route53.Model.Change ($action, $internalResourceRecordSet)
+
+    # Execute
+    Edit-R53ResourceRecordSet -HostedZoneId $internalHostedZone.id -ChangeBatch_Change $change 
+
+    #=============================================================================
+    # set DNS entry for *.probation.service.justice.gov.uk 
+    #=============================================================================
+    Set-Location ENV:
+    $PSJGUZoneName =  $environment.Value + ".probation.service.justice.gov.uk."
+    if (!$PSJGUZoneName)  {
+        Write-Host('Error - R53 Setup failed to zone name from PSJGUZoneName')
+        Exit 1
+    }
+    $PSJGUZone = Get-R53HostedZones | where Name -eq $PSJGUZoneName
+    if (!$PSJGUZone)  {
+        Write-Host('Error - R53 Setup failed to retrieve PSJGUZone hosted zone data')
+        Exit 1
+    }
+
+    # Set ResourceRecordSet for v1 of IAPS 
+    if($env:computername -eq 'sim-win-001') {
+        $resourceName = "iaps-admin." + $PSJGUZoneName
+    }
+   
+    # Set ResourceRecordSet for v2 of IAPS
+    if($env:computername -eq 'sim-win-002') {
+        $resourceName = "iaps-admin-v2." + $PSJGUZoneName
+    }
+    
+    $PSJGUResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+    $PSJGUResourceRecordSet.Name = $resourceName
+    $PSJGUResourceRecordSet.Type = "A"
+    $PSJGUResourceRecordSet.ResourceRecords = New-Object Amazon.Route53.Model.ResourceRecord ($LANIP)
+    $PSJGUResourceRecordSet.TTL = 300
+
+
+    # Set Action
+    if (((Get-R53ResourceRecordSet -HostedZoneId $PSJGUZone.id).ResourceRecordSets | where Name -eq $resourceName | measure).Count -eq 0)
+    {
+        $action = [Amazon.Route53.ChangeAction]::CREATE
+    }
+    else
+    {
+        $action = [Amazon.Route53.ChangeAction]::UPSERT
+    }
+
+    # Set Change 
+    $change = New-Object Amazon.Route53.Model.Change ($action, $PSJGUResourceRecordSet)
+
+    # Execute
+    Edit-R53ResourceRecordSet -HostedZoneId $PSJGUZone.id -ChangeBatch_Change $change 
+
 }
 catch [Exception] {
     Write-Host ('Failed to Update R53')
